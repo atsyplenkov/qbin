7bd5ccc55e254951daa8cf4890b1ef408878885a
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2025-04-27 20:56:41 +1200

    bench: add point encoding

--- benches/encode_point.rs
+++ benches/encode_point.rs
@@ -1,4 +1,5 @@
 use criterion::{Criterion, black_box};
+use h3o::{LatLng, Resolution};
 use qbin::Cell;
 
 const LAT: f64 = -41.28303675124842;
@@ -14,6 +15,12 @@ pub fn bench(c: &mut Criterion) {
     });
     group.bench_function("qbin", |b| {
         let index = Cell::from_point(LAT, LNG, 12);
+        b.iter(|| black_box(&index))
+    });
+
+    group.bench_function("h3o", |b| {
+        let latlng = LatLng::new(LAT, LNG).expect("Invalid coordinate");
+        let index = latlng.to_cell(Resolution::Twelve);
         b.iter(|| black_box(&index))
     });
 
