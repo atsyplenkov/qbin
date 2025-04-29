e5fb4c61fa3dfab4e0817fcff4391f032234c989
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2025-04-29 15:54:06 +1200

    bench: updated benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main};
 
+mod decode_point;
 mod encode_point;
 // mod get_cell_area;
 mod get_resolution;
@@ -8,7 +9,8 @@ criterion_group!(
     benches,
     get_resolution::bench,
     // get_cell_area::bench,
-    encode_point::bench
+    encode_point::bench,
+    decode_point::bench
 );
 
 criterion_main!(benches);
